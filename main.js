a4ca8009f14a884be6b172ee35f2f16dfb071176
MI1309 <imronm1309@gmail.com>
2025-08-06 06:56:41 +0700

    deleted file

--- main.js
+++
@@ -1,6 +0,0 @@
-const bars = document.querySelectorAll('.bar')
-bars.forEach(bar=>{
-    const teksLama = bar.textContent.trim()
-    const nilai = bar.dataset.value
-    bar.textContent = `${teksLama} ${nilai || 0}%`
-})
